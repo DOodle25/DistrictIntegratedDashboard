80f76683be8035530122171619ff09fe38ab547c
Dipen patel <pdipen135@gmail.com>
2024-09-06 10:18:06 +0530

    vite

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function education_validation(){
-    alert("hello")
-    var education = signupform.education.value;
-    alert(education)
-    if (education == "Secondary Education" || education == "Elementary Education"){
-        alert("inside")
-        signupform.educationaftermath.value = "you cant't submit this form cause your education is " + education;
-    }
-}
